29257bdd720c99018e0e49b8959bf561fd742aca
Son <sonpd@avr-solutions.com>
2019-08-09 14:17:57 +0700

    Asset

---
+++ ELJPrototype/Assets/ReceiveResult.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class ReceiveResult : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Debug.Log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ A");
+        }
+        else
+        {
+            // We do not have permission to use the microphone.
+            // Ask for permission or proceed without the functionality enabled.
+            Permission.RequestUserPermission(Permission.Microphone);
+            Debug.Log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ B");
+        }
+    }
+	
+    void onActivityResult(string recognizedText){
+        char[] delimiterChars = {'~'};
+        string[] result = recognizedText.Split(delimiterChars);
+
+        //You can get the number of results with result.Length
+        //And access a particular result with result[i] where i is an int
+        //I have just assigned the best result to UI text
+        GameObject.Find("Text").GetComponent<Text>().text = result[0];
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ELJPrototype/Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+    Button btn;
+	// Use this for initialization
+	void Start () {
+        
+        btn = GameObject.Find("Button").GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+	}
+
+    void runOnUiThread()
+    {
+        Debug.Log("I'm running on the Java UI thread!");
+        AndroidJavaClass pluginClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = pluginClass.GetStatic<AndroidJavaObject>("currentActivity");
+        activity.Call("promptSpeechInput");
+    }
+
+    void TaskOnClick()
+    {   
+        AndroidJavaClass pluginClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = pluginClass.GetStatic<AndroidJavaObject>("currentActivity");
+
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(runOnUiThread)); 
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
