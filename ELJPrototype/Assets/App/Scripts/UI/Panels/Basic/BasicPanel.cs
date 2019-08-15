8568c45965754fc083556f1bc3dbe5336f05c9ff
Son <sonpd@avr-solutions.com>
2019-08-15 17:19:37 +0700

    New Main menu

---
+++ ELJPrototype/Assets/App/Scripts/UI/Panels/Basic/BasicPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BasicPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject Root;
+
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+        _canvasGroup = Root.AddComponent<CanvasGroup>();
+        _canvasGroup.alpha = 0.0f;
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    public void SetActive(bool value)
+    {
+        Root.SetActive(value);
+        StopAllCoroutines();
+
+        StartCoroutine(value
+            ? Utils.FadeIn(_canvasGroup, 1.0f, 0.5f)
+            : Utils.FadeOut(_canvasGroup, 0.0f, 0.5f));
+    }
+}
